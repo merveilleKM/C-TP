2eed246747fde5858141202fc246cd765c8f7ae4
merveilleKM <merveillekouomo@gmail.com>
2026-01-13 11:12:34 +0100

    fin du TP4

--- TP4/main.cpp
+++ TP4/main.cpp
@@ -8,6 +8,11 @@
 
 int main()
 {
+    // creer le fichier // std::string monFichier("C:/Users/FTAB TECH/Documents/c++/OOP/TP/C-TP/TP4/Fichiers/toto.txt");
+    //std::fstream  monChemin("Fichiers/toto.txt");
+    //std::ofstream monChemin(monFichier.c_str());  //creer fichier si existe pas encore
+
+
     // Caesar2 ca("Les sanglots longs Des violons De l'automne Blessent mon coeur D'une Langueur Monotone", "Ohv#vdqjorwv#orqjv#Ghv#ylrorqv#Gh#o*dxwrpqh#Eohvvhqw#prq#frhxu#G*xqh#Odqjxhxu#Prqrwrqh",3);
     // std::cout << ca.encode();
     // std::cout << ca.decode();
